69c270754030950b2aa917450eb3ed4a9f0e53ed
Krista M <morales-k@outlook.com>
2022-11-20 12:16:36 -0500

    Added TaskVM
    Created view model for task logic.

---
+++ src/viewmodel/TaskVM.js
@@ -0,0 +1,71 @@
+/**
+ * Gets the most recent task.
+ * 
+ * @param {object} e - Event object.
+ * @param {function} setNewTask - Setter for the task.
+ */
+export function getTask(e, setNewTask) {
+    const value = e.currentTarget.value.trim();
+    
+    if (value !== "") {
+      setNewTask(value);
+    }
+}
+
+/**
+ * Marks task as completed.
+ * 
+ * @param {number} taskID - Task identifier.
+ * @param {Array} allTasks - List of all tasks.
+ * @param {Function} setTasks - Sets the task list.
+ */
+export function updateTask(taskID, allTasks, setTasks) {
+    const updatedTasks = allTasks;
+
+    updatedTasks.map(task => {
+      if (task.id === taskID) {
+        task.completed = true;
+      }
+    });
+
+    setTasks(updatedTasks);
+}
+
+/**
+ * Creates a new task object.
+ * 
+ * @param {string} newTask - Task description.
+ * @param {Array} allTasks - List of all tasks.
+ * @param {Function} setNewTask - Sets task description.
+ * @param {Function} setTasks - Sets the task list.
+ */
+export function addTask(newTask, allTasks, setNewTask, setTasks) {
+    if (newTask !== "") {
+      const updatedTasks = allTasks;
+      const taskObj = {
+        id: Math.floor((Math.random() * 1000) + 1),
+        title: newTask,
+        completed: false,
+        userId: 1
+      }
+    
+      updatedTasks.push(taskObj);
+      setTasks(updatedTasks);
+    }
+    
+    // Clear newTask & input.
+    setNewTask('');
+    document.querySelector('#taskInput').value = '';
+}
+
+/**
+ * Removes any task matching taskID.
+ * 
+ * @param {number} taskID - Task identifier.
+ * @param {Array} allTasks - List of tasks.
+ * @param {Function} setTasks - Sets the task list.
+ */
+export function deleteTask(taskID, allTasks, setTasks) {
+    const updatedTasks = allTasks.filter(task => task.id !== taskID);
+    setTasks(updatedTasks);
+}
