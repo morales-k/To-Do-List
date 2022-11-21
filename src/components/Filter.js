6a3f6fa65c78eb2159ad27c3e538a29ee3de78fb
Krista M <morales-k@outlook.com>
2022-11-21 16:47:18 -0500

    Added Filter
    Set up basic select to handle sorting.

---
+++ src/components/Filter.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Filter = () => {
+  return (
+    <select>
+        <option>All</option>
+        <option>Completed</option>
+        <option>Priority</option>
+        <option>Uncomplete</option>
+    </select>
+  )
+}
+
+export default Filter
