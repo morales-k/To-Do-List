413bb59d8d79b6b0d6e88f67de5ab01751624d00
Krista M <morales-k@outlook.com>
2022-11-21 19:38:17 -0500

    Updated filter & styles
    Filter displays # of tasks for a selected value. Added background image.

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -2,18 +2,26 @@ import React from 'react'
 import { filterTasks } from '../viewmodel/TaskVM'
 
 const Filter = (props) => {
-  const { tasks, setFilteredTasks } = props;
+  const { tasks, filteredTasks, setFilteredTasks, filterValue } = props;
 
   return (
-    <select 
+    <div className="filter-container">
+      <select 
+        id="filterTasks"
         aria-label="Filter tasks"
         name="filter" 
-        onChange={(e) => filterTasks(e.target.value, tasks, setFilteredTasks)}>
+        onChange={() => filterTasks(tasks, setFilteredTasks)}>
         <option>All</option>
         <option>Completed</option>
         <option>Priority</option>
         <option>Uncomplete</option>
-    </select>
+      </select>
+       {
+            filterValue !== 'all' ?
+            <p>{`${filteredTasks.length} ${filterValue} ${filteredTasks.length === 1 ? 'task' : 'tasks'}`}</p> :
+            <p>{`${tasks.length} ${tasks.length === 1 ? 'task' : 'tasks'}`}</p>
+        }
+    </div>
   )
 }
 
